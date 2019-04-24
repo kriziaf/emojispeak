ca816f98cf580d8016b657a74b937855534788f4
cfernando415 <ca.fernando415@gmail.com>
2019-04-24 18:22:15 -0400

    show most popular definition under random picked emoji

--- js/emoji.js
+++ js/emoji.js
@@ -11,10 +11,21 @@ document.addEventListener("DOMContentLoaded", async () => {
 function selectRandom() {
   const randomEmoji =
     emojipediaStore[Math.floor(Math.random() * emojipediaStore.length) + 1];
+
   const randomEmojiDiv = document.getElementById("emoji-random");
+
   randomEmojiDiv.firstElementChild.innerHTML =
     randomEmoji.name +
     " " +
     `<img src="./image/${randomEmoji.image}" alt="${randomEmoji.name}"/>`;
-  randomEmojiDiv.lastElementChild.innerText = randomEmoji.aliases;
+
+  let popularDefinition = randomEmoji.aliases[0];
+  for (let alias of randomEmoji.aliases) {
+    if (popularDefinition.votes < alias.votes) {
+      popularDefinition = alias;
+    }
+  }
+  randomEmojiDiv.lastElementChild.innerText = `${popularDefinition.alias}, ${
+    popularDefinition.votes
+  } votes`;
 }
