4a3d1334c2e626fa8ce8e324d88d8658760b89fe
cfernando415 <ca.fernando415@gmail.com>
2019-04-26 08:05:10 -0400

    finished loading various other aliases for emoji

--- js/emoji.js
+++ js/emoji.js
@@ -34,8 +34,34 @@ function selectRandom() {
 
 function loadEmojiMeaning(randomEmoji) {
   const emojiMeaningDiv = document.getElementById("emoji-meaning");
+  const aliasesDiv = document.getElementById("aliases");
 
   emojiMeaningDiv.firstElementChild.innerHTML = `${
     randomEmoji.name
   } <img src="./image/${randomEmoji.image}" alt="${randomEmoji.name}"/> <br/>`;
+
+  aliasesDiv.innerHTML = "";
+
+  aliasesDiv.innerHTML = randomEmoji.aliases
+    .sort((a, b) => a.votes - b.votes)
+    .map(
+      a => `<div class="col-xs-6">
+  <input
+    class="form-control input-lg js-tag-input"
+    readonly
+    type="text"
+    value="${a.alias}"
+  />
+</div>
+<div class="col-xs-1">
+  <p>${a.votes} votes</p>
+</div>
+<div class="col-xs-2">
+  <submit class="btn btn-primary btn-lg btn-block" type="submit">
+    Upvote
+  </submit>
+</div>
+<div class="col-xs-3">`
+    )
+    .join("");
 }
